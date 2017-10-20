cb0cfc5280d88246d50c1003ddf28fb03fb7c12c
Lucas Lofaro <lucasmlofaro@gmail.com>
2017-10-20 15:33:41 -0700

    first draft

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
+
+    $("nav #menu img").on('click', function (e) {
+        $('main').removeClass('fixed');
+        var id = $("#" + $(this).attr('ref'));
+        var section_top = id.offset().top;
+        $(window).scrollTop(section_top - 100);
+        e.preventDefault();
+    });
+
     $(window).scroll(function () {
-        const nav = $("nav");
-        const main = $('main');
-        const fixed = 'fixed';
-        const top = $(window).scrollTop();
+        var nav = $("#nav-container");
+        var main = $('main');
+        var fixed = 'fixed';
+        var top = $(window).scrollTop();
         top < nav.height() ? main.addClass(fixed) : main.removeClass(fixed)
     })
 });
